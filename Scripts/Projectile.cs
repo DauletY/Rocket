89ea3f1a492ba5ba4a311d50f268d898fa0dc9b2
DauletY <erekesh@outlook.com>
2020-07-18 23:25:39 +0500

    demo rocket

---
+++ Scripts/Projectile.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+using UnityEngine;
+
+namespace Assets.Rocket.Scripts
+{
+    public class Projectile : MonoBehaviour
+    {
+        public GameObject @object;
+        AudioSource audioSource;
+
+        private void Start()
+        {
+            audioSource = GetComponent(typeof(AudioSource)) as AudioSource;
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Ground"))
+            {
+                GameObject go = Instantiate(@object, transform.position, Quaternion.identity);
+                ParticleSystem particle = go.GetComponent<ParticleSystem>();
+                Destroy(particle.gameObject, 1f);
+                Destroy(this.gameObject, 1f);
+                audioSource.Play();
+                
+            }
+        }
+    }
+}
---
+++ Scripts/Rocket.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    public float time = 1;
+    public Transform _haed;
+    public GameObject _projectile;
+    public GameObject _projectileParticle;
+    public float _projectile_speed;
+    public float destroy_time;
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && Time.time >= time)
+        {
+
+            GameObject go = Instantiate(_projectile, _haed.position, Quaternion.identity);
+            Rigidbody rb = go.GetComponent<Rigidbody>();
+            rb.AddForce(new Vector3(_projectile_speed, _projectile_speed, _projectile_speed), ForceMode.Impulse);
+
+            GameObject _go = Instantiate(_projectileParticle, _haed.position, Quaternion.identity);
+            Transform _transform = _go.GetComponent<Transform>();
+            ParticleSystem particle = _transform.GetComponent<ParticleSystem>();
+            Destroy(_transform.gameObject, destroy_time);
+            audioSource.Play();
+        }
+    }
+}
